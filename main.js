c07d8310c1deaaa3317fd356e24dee5c34eab85e
GabrieleOliveira-ai <gs2371797@gmail.com>
2025-06-04 19:03:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,14 @@
-class MobileNavbar {
-  constructor(mobileMenu, navList, navLinks) {
-    this.mobileMenu = document.querySelector(mobileMenu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
+const Clock = document.getElementById("relogio")
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  animateLinks() {
-    this.navLinks.forEach((link, index) => {
-      link.style.animation
-        ? (link.style.animation = "")
-        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.3
-          }s`);
-    });
-  }
-
-  handleClick() {
-    this.navList.classList.toggle(this.activeClass);
-    this.mobileMenu.classList.toggle(this.activeClass);
-    this.animateLinks();
-  }
-
-  addClickEvent() {
-    this.mobileMenu.addEventListener("click", this.handleClick);
-  }
-
-  init() {
-    if (this.mobileMenu) {
-      this.addClickEvent();
-    }
-    return this;
-  }
+function ClockUpdate (){
+  const data = new Date ()
+  const horas = data.getHours()
+  const minutos = data.getMinutes()
+  const segundos = data.getSeconds() <= 9 ? "0" + data.getSeconds() : data.getSeconds()
+  
+  Clock.innerHTML = `${horas}: ${minutos}: ${segundos}`
 }
 
-const mobileNavbar = new MobileNavbar(
-  ".mobile-menu",
-  ".nav-list",
-  ".nav-list li",
-);
-mobileNavbar.init();
+
+setInterval(ClockUpdate, 1000)
+
